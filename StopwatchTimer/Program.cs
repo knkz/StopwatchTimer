21654d240c74e1b21ac099848877e5e2c16b4d13
knkz <kano@Bittenfeld.localdomain>
2022-07-20 21:48:54 +0900

    add StopwatchTimer

---
+++ StopwatchTimer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Timers;
+
+namespace PerformanceTimer
+{
+    internal class Program
+    {
+        static System.Timers.Timer _timer;
+        static StopwatchTimer _pTimer;
+        static int _count;
+
+        static void Main(string[] args)
+        {
+            //_timer = new System.Timers.Timer
+            //{
+            //    AutoReset = true,
+            //    Interval = 10
+            //};
+            //_timer.Elapsed += Timer_Elapsed;
+            //_timer.Start();
+            _pTimer = new StopwatchTimer(10, () => ElapsedImpl());
+            _pTimer.Start();
+            var _ = Console.ReadLine();
+        }
+
+        private static void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            ElapsedImpl();
+        }
+
+        private static void ElapsedImpl()
+        {
+            if (_count > 100)
+            {
+                _timer?.Stop();
+                _pTimer?.Stop();
+                return;
+            }
+            Console.WriteLine(DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.ffff"));
+            _count++;
+        }
+    }
+}
---
+++ StopwatchTimer/StopwatchTimer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceTimer
+{
+    internal class StopwatchTimer
+    {
+        private const int MINIMUM_DELAY = 30;
+
+        private Stopwatch _stopWatch = new();
+
+        private bool _enabled;
+
+        private Action _callback;
+
+        private int _interval;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="interval"></param>
+        /// <param name="callback"></param>
+        public StopwatchTimer(int interval, Action callback)
+        {
+            _interval = interval;
+            _callback = callback;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Start()
+        {
+            _enabled = true;
+            StartTimer();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Stop()
+        {
+            _enabled = false;
+        }
+
+        private void StartTimer()
+        {
+            _ = Task.Run(async () =>
+            {
+                _stopWatch.Restart();
+                while (_enabled)
+                {
+                    var msec = _stopWatch.ElapsedMilliseconds;
+                    var rest = _interval - (int)(msec / _interval);
+
+                    if (rest > MINIMUM_DELAY)
+                    {
+                        await Task.Delay(rest - MINIMUM_DELAY);
+                    }
+
+                    while (true)
+                    {
+                        if (_stopWatch.ElapsedMilliseconds >= msec + _interval)
+                        {
+                            break;
+                        }
+                        Thread.Sleep(0);
+                    }
+                    _callback.Invoke();
+                }
+                _stopWatch.Stop();
+            });
+        }
+    }
+}
